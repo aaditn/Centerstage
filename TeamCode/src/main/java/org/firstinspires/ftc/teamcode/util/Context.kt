77a5f3b14705136d02aa8c50c8eb0db65e623f83
jiru-shan <90534758+jiru-shan@users.noreply.github.com>
2023-11-29 14:20:58 -0800

    fixing camera loading issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Context.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Context.kt
@@ -1,16 +1,23 @@
 package org.firstinspires.ftc.teamcode.util
 
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 
 object Context
 {
     @JvmField var tel: MultipleTelemetry? = null
     @JvmField var isTeamRed: Boolean = true
     @JvmField var debug: Int = 0
+    @JvmField var opmode: LinearOpMode? = null;
+    @JvmField var isTele: Boolean = true
 
     @JvmStatic fun updateValues()
     {
-
+        if (opmode!!.javaClass.isAnnotationPresent(Autonomous::class.java))
+        {
+            isTele=false
+        }
     }
 
     @JvmStatic fun resetValues()

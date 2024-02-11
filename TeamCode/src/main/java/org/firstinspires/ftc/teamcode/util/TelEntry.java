a84ec02c4901bbb294220cab221a320512976b87
jiru-shan <90534758+jiru-shan@users.noreply.github.com>
2024-02-10 20:59:46 -0800

    log timing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TelEntry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TelEntry.java
@@ -8,9 +8,6 @@ public class TelEntry
     public String tag;
     public Object data;
     public boolean logged=false;
-    public int logTimer=500;
-    private long lastTimeLogged=0;
-
 
     public TelEntry(String tag, Object data)
     {
@@ -23,23 +20,5 @@ public class TelEntry
         this.tag=tag;
         this.data=data;
         this.logged=logged;
-    }
-
-    public TelEntry(String tag, Object data, boolean logged, int logTimer)
-    {
-        this.tag=tag;
-        this.data=data;
-        this.logged=logged;
-        this.logTimer=logTimer;
-    }
-
-    public void log()
-    {
-        long currentTime=SystemClock.elapsedRealtime();
-        if(currentTime-lastTimeLogged>logTimer)
-        {
-            Log.d(tag, data.toString());
-            lastTimeLogged=currentTime;
-        }
     }
 }

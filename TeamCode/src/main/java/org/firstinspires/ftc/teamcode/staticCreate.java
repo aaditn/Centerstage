664287c957eea9cd0541760b0d9805323ff3f095
kots727 <90489449+kots727@users.noreply.github.com>
2024-02-17 11:16:37 -0800

    Update staticCreate.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/staticCreate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/staticCreate.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.ftccommon.external.OnCreate;
+import org.firstinspires.ftc.robotcore.internal.opmode.RegisteredOpModes;
 import org.firstinspires.ftc.teamcode.auto_paths.Batch;
 import org.firstinspires.ftc.teamcode.util.NamedTrajectory;
 
@@ -25,7 +26,22 @@ public class staticCreate implements OpModeManagerImpl.Notifications {
                 +", Mark: " + batchInit.toString());
         List<Pose2d> batchInitPT2 = Batch.allStarts;
         RobotLog.e("All Start Pos Loaded in: "+timeToInit.seconds()+", Mark: " +batchInitPT2.toString());
+        while(true){
 
+            if(RegisteredOpModes.getInstance().getExternalLibrariesChanged()){
+                ElapsedTime timeToInit3 = new ElapsedTime();
+                RobotLog.e("Static Force Initialization Begins");
+                List<NamedTrajectory[][]> batchInit6  = Batch.allTrajectories;
+                RobotLog.e("All Trajectories Loaded in: "+ timeToInit.seconds()
+                        +", Mark: " + batchInit.toString());
+                List<Pose2d> batchInitPT26 = Batch.allStarts;
+                RobotLog.e("All Start Pos Loaded in: "+timeToInit.seconds()+", Mark: " +batchInitPT2.toString());
+            }
+            ElapsedTime x  = new ElapsedTime();
+            while(x.seconds()<1){
+                
+            }
+        }
     }
 
     @Override
